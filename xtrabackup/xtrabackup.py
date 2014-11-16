bc8d3009b976be2de30a2d097ccffad5693abe46
Anthony Lapenna <lapenna.anthony@gmail.com>
2014-11-16 18:53:25 +0100

    Replaced check_call by check_output to retrieve command output on error.

--- xtrabackup/xtrabackup.py
+++ xtrabackup/xtrabackup.py
@@ -3,22 +3,22 @@ import subprocess
 
 def exec_filesystem_backup(user, password, threads, backup_directory):
     if password:
-        subprocess.check_call([
+        subprocess.check_output([
             'innobackupex',
             '--user=' + user,
             '--password=' + password,
             '--parallel=' + threads,
             '--no-lock',
             '--no-timestamp',
-            backup_directory])
+            backup_directory], stderr=subprocess.STDOUT)
     else:
-        subprocess.check_call([
+        subprocess.check_output([
             'innobackupex',
             '--user=' + user,
             '--parallel=' + threads,
             '--no-lock',
             '--no-timestamp',
-            backup_directory])
+            backup_directory], stderr=subprocess.STDOUT)
 
 
 def exec_backup_preparation(backup_directory):
