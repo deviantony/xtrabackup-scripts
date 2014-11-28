5cf1bc8a697a439d44ac6cf348f59a33d3cc96fe
Anthony Lapenna <lapenna.anthony@gmail.com>
2014-11-28 17:49:47 +0100

    Added class LogManager

---
+++ xtrabackup/logManager.py
@@ -0,0 +1,12 @@
+import logging
+
+
+class LogManager:
+
+    def attach_file_handler(logger, log_file):
+        handler = logging.FileHandler(log_file)
+        handler.setLevel(logging.INFO)
+        formatter = logging.Formatter(
+            '%(asctime)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
