736628b427f0768748aa40530f80386e3ffe330d
tony <alapenna@workit.fr>
2014-12-02 11:37:34 +0100

    Updated setup.py: added python 2 and python 2.7 support.

--- setup.py
+++ setup.py
@@ -32,6 +32,8 @@ setup(
 
         'License :: OSI Approved :: Apache Software License',
 
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
     ],
