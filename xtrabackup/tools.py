466e14b75f5f16340638fd261c0cd0b9225d58ed
Anthony Lapenna <lapenna.anthony@gmail.com>
2014-12-14 18:05:59 +0100

    Added logging.

--- xtrabackup/tools.py
+++ xtrabackup/tools.py
@@ -202,49 +202,114 @@ class RestoreTool:
         self.logger.debug("Temporary workdir: " + self.workdir)
 
     def stop_service(self):
-        command_executor.exec_manage_service('mysql', 'stop')
-        print('Service stop: OK')
+        try:
+            command_executor.exec_manage_service('mysql', 'stop')
+        except:
+            self.logger.error(
+                'Unable to manage MySQL service.',
+                exc_info=True)
+            self.clean()
+            raise
 
     def clean_data_dir(self):
-        filesystem_utils.clean_directory('/var/lib/mysql')
-        print('Datadir clean: OK')
+        try:
+            filesystem_utils.clean_directory('/var/lib/mysql')
+        except:
+            self.logger.error(
+                'Unable to clean MySQL data directory.',
+                exc_info=True)
+            self.clean()
+            raise
 
     def restore_base_backup(self, archive_path):
-        filesystem_utils.extract_archive(archive_path, '/var/lib/mysql')
-        command_executor.exec_backup_preparation('/var/lib/mysql', True)
-        print('Restore base: OK')
+        self.stop_watch.start_timer()
+        try:
+            filesystem_utils.extract_archive(archive_path, '/var/lib/mysql')
+            command_executor.exec_backup_preparation('/var/lib/mysql', True)
+        except CalledProcessError as e:
+            self.logger.error(
+                'An error occured during the base backup restoration process.',
+                exc_info=True)
+            self.logger.error(
+                'Command output: %s', e.output.decode(stdout.encoding))
+            self.clean()
+            raise
+        self.logger.info("Base backup restoration time: %s - Duration: %s",
+                         self.stop_watch.stop_timer(),
+                         self.stop_watch.duration_in_seconds())
 
     def restore_incremental_backups(self, incremental_archive):
-        repository, archive_name = filesystem_utils.split_path(
-            incremental_archive)
-        incremental_target = int(archive_name.split('_')[1])
-        for step in range(1, incremental_target + 1):
-            self.restore_incremental_backup(repository, step)
+        try:
+            repository, archive_name = filesystem_utils.split_path(
+                incremental_archive)
+            incremental_target = int(archive_name.split('_')[1])
+            for step in range(1, incremental_target + 1):
+                self.restore_incremental_backup(repository, step)
+        except:
+            self.logger.error(
+                'An error occured during the incremental\
+                 backups restoration process.',
+                exc_info=True)
+            self.clean()
+            raise
 
     def restore_incremental_backup(self, archive_repository, incremental_step):
-        prefix = ''.join(['inc_', str(incremental_step), '_'])
-        backup_archive = filesystem_utils.get_prefixed_file_in_dir(
-            archive_repository, prefix)
-        extracted_archive_path = ''.join([self.workdir, '/',
-                                          prefix, 'archive'])
-        filesystem_utils.mkdir_path(extracted_archive_path, 0o755)
-        filesystem_utils.extract_archive(backup_archive,
-                                         extracted_archive_path)
-        command_executor.exec_incremental_preparation('/var/lib/mysql',
-                                                      extracted_archive_path)
-        print('Restore inc archive: OK')
+        self.stop_watch.start_timer()
+        try:
+            prefix = ''.join(['inc_', str(incremental_step), '_'])
+            backup_archive = filesystem_utils.get_prefixed_file_in_dir(
+                archive_repository, prefix)
+            extracted_archive_path = ''.join([self.workdir, '/',
+                                              prefix, 'archive'])
+            filesystem_utils.mkdir_path(extracted_archive_path, 0o755)
+            filesystem_utils.extract_archive(backup_archive,
+                                             extracted_archive_path)
+            command_executor.exec_incremental_preparation(
+                '/var/lib/mysql',
+                extracted_archive_path)
+        except:
+            self.logger.error(
+                'An error occured during an incremental backup restoration.',
+                exc_info=True)
+            self.clean()
+            raise
+        self.logger.info("Incremental step #%s restoration time: %s\
+ - Duration: %s",
+                         incremental_step,
+                         self.stop_watch.stop_timer(),
+                         self.stop_watch.duration_in_seconds())
 
     def prepare_data_dir(self):
-        command_executor.exec_backup_preparation('/var/lib/mysql', False)
-        print('Datadir prepa: OK')
+        try:
+            command_executor.exec_backup_preparation('/var/lib/mysql', False)
+        except:
+            self.logger.error(
+                'An error occured during the backup final preparation.',
+                exc_info=True)
+            self.clean()
+            raise
+        self.logger.info("Backup final preparation time: %s - Duration: %s",
+                         self.stop_watch.stop_timer(),
+                         self.stop_watch.duration_in_seconds())
 
     def set_data_dir_permissions(self):
-        command_executor.exec_chown('mysql', 'mysql', '/var/lib/mysql')
-        print('Datadir permissions: OK')
+        try:
+            command_executor.exec_chown('mysql', 'mysql', '/var/lib/mysql')
+        except:
+            self.logger.error('Unable to reset MySQL data dir permissions.',
+                              exc_info=True)
+            self.clean()
+            raise
 
     def start_service(self):
-        command_executor.exec_manage_service('mysql', 'start')
-        print('Service start: OK')
+        try:
+            command_executor.exec_manage_service('mysql', 'start')
+        except:
+            self.logger.error(
+                'Unable to manage MySQL service.',
+                exc_info=True)
+            self.clean()
+            raise
 
     def clean(self):
         filesystem_utils.delete_directory(self.workdir)
