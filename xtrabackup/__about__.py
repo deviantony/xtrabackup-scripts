d8b7d82b462eede329891de99d46356dc851683e
Anthony Lapenna <lapenna.anthony@gmail.com>
2014-12-22 18:24:14 +0100

    Updated version number.

--- xtrabackup/__about__.py
+++ xtrabackup/__about__.py
@@ -7,7 +7,7 @@ __title__ = "pyxtrabackup"
 __summary__ = "Percona Xtrabackup python wrapper"
 __uri__ = "https://github.com/deviantony/xtrabackup-scripts"
 
-__version__ = "3.0.0b2"
+__version__ = "3.0.0b3"
 
 __author__ = "Anthony Lapenna"
 __email__ = "lapenna.anthony@gmail.com"
