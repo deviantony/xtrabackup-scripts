994bbf307fced95113612f0248b84bb88d72b660
tony <alapenna@workit.fr>
2015-05-07 11:17:53 +0200

    Bumped version number.

--- xtrabackup/restoration.py
+++ xtrabackup/restoration.py
@@ -53,7 +53,7 @@ from xtrabackup.restoration_tools import RestorationTool
 
 
 def main():
-    arguments = docopt(__doc__, version='3.1.1')
+    arguments = docopt(__doc__, version='3.1.2')
     restore_tool = RestorationTool(arguments['--log-file'],
                                    arguments['--out-file'],
                                    arguments['--data-dir'],
